08a9b58d2705f0c81f13d8bbab5f3e67392a29db
MCBANKSKE <mcbankske@gmail.com>
2026-05-28 18:49:58 +0300

    Fixed the uninstall.php warnings by adding @codingStandardsIgnoreLine comments for the three database query warnings

--- uninstall.php
+++ uninstall.php
@@ -14,4 +14,4 @@ delete_option( 'paynexus_settings' );
 // Drop payments table.
 global $wpdb;
 $paynexus_table = $wpdb->prefix . 'paynexus_payments';
-$wpdb->query( $wpdb->prepare( "DROP TABLE IF EXISTS `%s`", $paynexus_table ) );
+$wpdb->query( $wpdb->prepare( "DROP TABLE IF EXISTS `%s`", $paynexus_table ) ); // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.SchemaChange
